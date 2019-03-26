08cc432d3f6f4a7e4fbbb43713ddcf3d1a513470
Alex <soliaris2012@mail.ru>
2019-03-26 22:24:08 +0300

    second try

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+    logger->info("Wow, version {} was started", version());
 
     return 0;
 }
