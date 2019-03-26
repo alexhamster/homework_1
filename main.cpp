30538c787026fabb9863f9a0009e89746dc4ef75
alex <alex@mail.ru>
2019-03-27 02:25:17 +0500

    changed project name from helloworld_cli to helloworld

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("Wow, version {} was started", version());
+    logger->info("build  {}, Hello World!", version());
 
     return 0;
 }
